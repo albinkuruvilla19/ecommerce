62deea4d405f4e9186fc73d9741cc5e6f05d3754
Albin <albinkuruvilla1926@gmail.com>
2024-10-01 14:51:06 +0530

    Added Structure of Home Page and added some products

--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Products
 # Create your views here.
+
+def index(request):
+    allproducts = Products.objects.filter(status=0)
+    return render(request,'index.html',{"allproducts":allproducts})
